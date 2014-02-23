2d969414c26d6df5ffd60d7153fc1bcb3493d7e3
Mason DeCamillis <md2477@gmail.com>
2014-02-23 11:35:15 -0500

    Added plot with smoothing line and an example of SVG output

--- 01 Demos/graphics.jl
+++ 01 Demos/graphics.jl
@@ -1,6 +1,30 @@
+# Demo of Cairo and Gadfly packages to use for plotting (similar to R's ggplot)
+# Code can be found on this page of the Gadfly documentation:
+# http://dcjones.github.io/Gadfly.jl/#drawing-to-backends
+
 using Cairo, Gadfly
 plot_dir = "C:/Users/Mason/Dropbox/Projects/Julia/Triangle Julia Users Group/TriangleJuliaUsers/01 Demos/Graphics"
 
 # Plot two random vectors of length 10
 plot1 = plot(x = rand(10), y = rand(10))
 draw(PNG("$plot_dir/plot1.png", 6inch, 4inch), plot1)
+
+# Add a smoothing line:
+plot2 = plot(
+    x = rand(10),
+    y = rand(10),
+    Geom.point,
+    Geom.smooth,
+    Guide.xlabel("This is the x-axis"),
+    Guide.ylabel("This is the y-axis"),
+    Guide.title("This is the overall plot title"))
+
+draw(PNG("$plot_dir/plot2.png", 6inch, 4inch), plot2)
+
+# Export to SVG/PNG/PDF/PS/D3
+plot_function = SVG
+plot_suffix = "svg"
+draw(plot_function("$plot_dir/plot3.$plot_suffix", 6inch, 4inch), plot2)
+
+
+
