e9247ac2973d57bcd9993dc1882aad52f2d2019c
Mason DeCamillis <md2477@gmail.com>
2014-02-23 11:48:11 -0500

    Added example with a DataFrame, and PDF output

--- 01 Demos/graphics.jl
+++ 01 Demos/graphics.jl
@@ -2,14 +2,14 @@
 # Code can be found on this page of the Gadfly documentation:
 # http://dcjones.github.io/Gadfly.jl/#drawing-to-backends
 
-using Cairo, Gadfly
+using Cairo, Gadfly, RDatasets
 plot_dir = "C:/Users/Mason/Dropbox/Projects/Julia/Triangle Julia Users Group/TriangleJuliaUsers/01 Demos/Graphics"
 
-# Plot two random vectors of length 10
+# Example 1: Plot two random vectors of length 10
 plot1 = plot(x = rand(10), y = rand(10))
 draw(PNG("$plot_dir/plot1.png", 6inch, 4inch), plot1)
 
-# Add a smoothing line:
+# Example 2: Add a smoothing line:
 plot2 = plot(
     x = rand(10),
     y = rand(10),
@@ -21,10 +21,16 @@ plot2 = plot(
 
 draw(PNG("$plot_dir/plot2.png", 6inch, 4inch), plot2)
 
-# Export to SVG/PNG/PDF/PS/D3
+# Example 3: Export to SVG/PNG/PDF/PS/D3
 plot_function = SVG
 plot_suffix = "svg"
 draw(plot_function("$plot_dir/plot3.$plot_suffix", 6inch, 4inch), plot2)
 
+# Example 4: Use a DataFrame
+plot4 = plot(
+    data("car", "SLID"),
+    x       = "Wages",
+    color   = "Language",
+    Geom.histogram)
 
-
+draw(PDF("$plot_dir/plot4.pdf", 6inch, 4inch), plot4)
