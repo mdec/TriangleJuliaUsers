02a492e79c402961448677e2d4cfb94d8bc7b6d2
Mason DeCamillis <md2477@gmail.com>
2014-02-24 07:48:27 -0500

    Resetting files to status before merge conflict

--- 01 Demos/parallel.jl
+++ 01 Demos/parallel.jl
@@ -1,7 +1,6 @@
 # Julia offers very simple parallel programming techniques.
 
 # Looping:
-<<<<<<< HEAD
 input = Int[1:1000]
 
 # Basic for loop
@@ -21,26 +20,3 @@ sum_squares = @parallel (+) for i in input
     i ^ 2
 end
 println(sum_squares)
-
-=======
-    input = Int[1:1000]
-
-    # Basic for loop
-    println(">> Basic loop:")
-    sum_squares = int64(0)
-
-    for i in input
-        sum_squares += i ^ 2
-    end
-    println(sum_squares)
-
-    # Parallel loop
-    println("Parallel loop time:")
-    sum_squares = int64(0)
-
-    sum_squares = @parallel (+) for i in input
-        i ^ 2
-    end
-    println(sum_squares)
-
->>>>>>> c14f8ce380a9c261f689800a59b19f3de3d4cbd8
--- 01 Demos/parallel2.jl
+++ 01 Demos/parallel2.jl
@@ -1,12 +1,9 @@
 # The following is an example adapted from http://www.blog.juliaferraioli.com/2014/02/julia-on-google-compute-engine-parallel.html
-<<<<<<< HEAD
 # This file should be called/run from the command line with multiple processors, not sourced into the JuliaStudio console.
 
 # To use multiple processor cores, start Julia like this:
 # julia -p [2/4/whatever number of processors you have] "path to this file"
 
-=======
->>>>>>> c14f8ce380a9c261f689800a59b19f3de3d4cbd8
 
 @parallel for i = 1:10
     # spawn the process
