dda70a57de852f9ce44ec56a28beb89ebb66e12b
Mason DeCamillis <md2477@gmail.com>
2014-02-24 07:45:20 -0500

    Resetting indention and println for first parallel demo program

--- 01 Demos/parallel.jl
+++ 01 Demos/parallel.jl
@@ -1,23 +1,23 @@
 # Julia offers very simple parallel programming techniques.
 
 # Looping:
-    input = Int[1:1000]
+input = Int[1:1000]
 
-    # Basic for loop
-    println(">> Basic loop:")
-    sum_squares = int64(0)
+# Basic for loop
+println(">> Basic loop:")
+sum_squares = int64(0)
 
-    for i in input
-        sum_squares += i ^ 2
-    end
-    println(sum_squares)
+for i in input
+    sum_squares += i ^ 2
+end
+println(sum_squares)
 
-    # Parallel loop
-    println("Parallel loop time:")
-    sum_squares = int64(0)
+# Parallel loop
+println("Parallel loop:")
+sum_squares = int64(0)
 
-    sum_squares = @parallel (+) for i in input
-        i ^ 2
-    end
-    println(sum_squares)
+sum_squares = @parallel (+) for i in input
+    i ^ 2
+end
+println(sum_squares)
 
